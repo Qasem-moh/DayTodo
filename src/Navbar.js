e3806fe4bf8eeaf3b59772680e51b1a7079e79e1
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-01-11 14:22:08 +0300

    react

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const  Navbar= () => {
+    return ( 
+        <nav className="navbar navbar-">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                </div>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
