ca24a78d280cb8a2c378d49cd07255cff6d6f2d8
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-01-11 17:13:41 +0300

    react

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
+
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h1>Home</h1>
-        </div>
-     );
-}
- 
+
+  const[name,setName]=useState("Qasem")
+  const[age,setAge]=useState(30)
+
+  const handleChangeName=()=>{
+    setName("ALI")
+  }
+
+  const handleAge=()=>{
+      const tr= prompt('Enter your age ')
+      setAge(tr)
+  }
+
+  return (
+    <div className="content">
+      <h1>Home</h1>
+      <h2>Hello {name} and your age is {age}</h2>
+      <button onClick={handleChangeName}>Change name</button>
+      <button onClick={handleAge}>Change age</button>
+    </div>
+  );
+};
+
 export default Home;
