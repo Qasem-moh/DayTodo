4ed66511a060937ce6ecb242cbc34cffb3b53acc
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-01-13 13:55:10 +0300

    react

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,8 @@ import {Link} from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Togo20</h1>
+      <h1> Togo20 </h1>
+        <img src="https://i.ibb.co/f9h62Fk/Your-paragraph-text-222.png" className="logo x-5"/>
       <div className="links">
         {/*  Links*/}
         <Link to="/">Home</Link>
