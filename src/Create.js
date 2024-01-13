d599b38b279b739cb9554295c793121c30e809fb
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-01-13 16:09:25 +0300

    react

--- src/Create.js
+++ src/Create.js
@@ -1,19 +1,44 @@
+import {useState} from "react";
+
 const Create = () => {
+    const [title,setTiltle]=useState('');
+    const [body,setBody]=useState('');
+    const [author,setAuthor]=useState('other');
+    const [typeB,setTypeB]=useState('other')
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
             <label>Blog title: </label>
-            <input type="text" required/>
+            <input
+                type="text"
+                value={title}
+                required
+            onChange={(e)=>setTiltle(e.target.value)}
+            />
 
             <label>Blog body: </label>
-            <textarea required>
+            <textarea
+                required
+                value={body}
+                onChange={(e)=>setBody(e.target.value)}
+            >
             </textarea>
 
             <label>Blog author</label>
-            <input type="text" required/>
+            <input
+                type="text"
+                value={author}
+                required
+                onChange={(e)=>setAuthor(e.target.value)}
+
+            />
 
             <label>Blog type</label>
-            <select>
+            <select
+                value={typeB}
+                onChange={(e)=>setTypeB(e.target.value)}
+
+            >
                 <option value="since">Since</option>
                 <option value="history">History</option>
                 <option value="media">Media</option>
